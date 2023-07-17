036068d96a8c57f1c5853068cb4cbe0fe6b8ad1d
roq-ai <>
2023-07-17 15:04:18 +0000

    Initial commit

---
+++ src/interfaces/company/index.ts
@@ -0,0 +1,31 @@
+import { CustomerInterface } from 'interfaces/customer';
+import { EmployeeInterface } from 'interfaces/employee';
+import { UserInterface } from 'interfaces/user';
+import { GetQueryInterface } from 'interfaces';
+
+export interface CompanyInterface {
+  id?: string;
+  description?: string;
+  image?: string;
+  name: string;
+  created_at?: any;
+  updated_at?: any;
+  user_id: string;
+  tenant_id: string;
+  customer?: CustomerInterface[];
+  employee?: EmployeeInterface[];
+  user?: UserInterface;
+  _count?: {
+    customer?: number;
+    employee?: number;
+  };
+}
+
+export interface CompanyGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  description?: string;
+  image?: string;
+  name?: string;
+  user_id?: string;
+  tenant_id?: string;
+}
